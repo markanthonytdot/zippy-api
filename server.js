19332a3066789e600b1737cea4cb21c1f85bb021
markanthonytdot <s.mark@me.com>
2026-01-04 11:58:05 -0500

    fix trust proxy for real client IP

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const { Pool } = require("pg");
 const app = express();
 
-app.set("trust proxy", 1);
+app.set("trust proxy", true);
 app.use(helmet());
 app.use(express.json({ limit: "256kb" }));
 app.use(express.urlencoded({ extended: false, limit: "64kb" }));
